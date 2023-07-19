c3dc47582593aeb73479537b27d87363db164534
nicoqk <ncuccarese@cepdigital.com>
2023-07-18 22:34:48 -0300

    desafio2 v1

--- ProductManager.js
+++ ProductManager.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
 
+const fs = require ("fs");
 class ProductManager {
     constructor() {
         this.products = [];
@@ -57,59 +57,59 @@ class ProductManager {
     saveProducts() {
         fs.writeFileSync(this.path, JSON.stringify(this.products));
     }
-}
 
-updateProduct(id, product) {
-    this.products = this.getProducts();
-    let pos = this.products.findIndex(item => item.id === id);
 
-    if (pos > -1) {
-        this.products[pos].title = product.title;
-        this.products[pos].description = product.description;
-        this.products[pos].price = product.price;
-        this.products[pos].thumbnail = product.thumbnail;
-        this.products[pos].code = product.code;
-        this.products[pos].stock = product.stock;
-        this.saveProducts();
-        console.log("Product updated!");
-    } else {
-        console.log("Not found!");
+    updateProduct(id, product) {
+        this.products = this.getProducts();
+        let pos = this.products.findIndex(item => item.id === id);
+
+        if (pos > -1) {
+            this.products[pos].title = product.title;
+            this.products[pos].description = product.description;
+            this.products[pos].price = product.price;
+            this.products[pos].thumbnail = product.thumbnail;
+            this.products[pos].code = product.code;
+            this.products[pos].stock = product.stock;
+            this.saveProducts();
+            console.log("Product updated!");
+        } else {
+            console.log("Not found!");
+        }
+
     }
-}
 
-deleteProduct(id) {
-    this.products = this.getProducts();
-    let pos = this.products.findIndex(item => item.id === id);
+    deleteProduct(id) {
+        this.products = this.getProducts();
+        let pos = this.products.findIndex(item => item.id === id);
 
-    if (pos > -1) {
-        this.products.splice(pos, 1); (0,1)
-        this.saveProducts();
-        console.log("Product #" + id + " deleted!");
-    } else {
-        console.log("Not found!");
+        if (pos > -1) {
+            this.products.splice(pos, 1); (0,1)
+            this.saveProducts();
+            console.log("Product #" + id + " deleted!");
+        } else {
+            console.log("Not found!");
+        }
     }
+
 }
 
 
 
 
+//pruebas del codigo
 
 const PM = new ProductManager();
 
+//crea productos y el archivo Products.json
 PM.addProduct({title:"Guitarra Fender Telecaster", description:"Tele 52", price:3500, thumbnail:"sin imagen", code:"TELE", stock:10});
-
 PM.addProduct({title:"Guitarra Fender Stratocaster", description:"Start AVRI ", price:3000, thumbnail:"sin imagen", code:"STRAT", stock:10});
-
 PM.addProduct({title:"Guitarra Gibson Les Paul", description:"Les Paul Standar", price:3500, thumbnail:"sin imagen", code:"LP", stock:10});
-
 PM.addProduct({title:"Guitarra Gibson SG", description:"SG Standar", price:3500, thumbnail:"sin imagen", code:"SG", stock:10});
 
-console.log(PM.getProducts());
-console.log(PM.getProductById(3));
-console.log(PM.getProductById(1));
 
-//console.log(PM.getProductById(3));
-//console.log(PM.getProductById(1));
-//PM.deleteProduct(1);
-//PM.updateProduct(2, {title:"Chaleco Puffer Ebruck Topo 4", description:"3 cuotas sin interes de $54.330ARS", price:58990, thumbnail:"https://vcp.com.ar/cdn/shop/files/Topo6_81943047-662b-4e1c-82ae-9e26fb212505.jpg?v=1688741569&width=700", code:"CHALECO 4", stock:18})
-//console.log(PM.getProducts());
+//console.log(PM.getProducts()); //Obtengo todos los productos
+//console.log(PM.getProductById(4)); /Obtengo productos por ID
+//console.log(PM.getProductById(2)); /Obtengo productos por ID
+
+//PM.deleteProduct(1); //elimino productos por id
+//PM.updateProduct(2, {title:"Guitarra Fender Jaguar", description:"Jaguar", price:58990, thumbnail:"Sin imagen", code:"FJ", stock:18}) //actualizo productos
